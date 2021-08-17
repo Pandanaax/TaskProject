6a9a98962762e6d10eb781a1e9f4f02759adde5a
Randa <baliranda@gmail.com>
2021-08-17 14:40:33 +0200

    add boostrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import "./App.css";
 import RouterTask from "./routing";
+import Header from "./header/Header";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <RouterTask />
       </div>
     </Router>
